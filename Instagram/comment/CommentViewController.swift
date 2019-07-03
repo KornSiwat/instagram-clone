0c7f01e6f2a0c30c6acea664378239009395eec1
KornSiwat <ponpued@gmail.com>
2019-07-03 23:17:31 +0700

    added keyboardObserver

--- Instagram/comment/CommentViewController.swift
+++ Instagram/comment/CommentViewController.swift
@@ -9,25 +9,42 @@
 import UIKit
 
 class CommentViewController: UIViewController {
+    @IBOutlet weak var commentBarProfileImage: UIImageView!
+
     var comments: [Comment]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        commentBarProfileImage.roundedImage()
+        addKeyboardObserver()
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        removeKeyboardObserver()
     }
-    */
+}
 
+// MARK: - Keyboard Observer
+extension CommentViewController {
+    func addKeyboardObserver() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: Selector(("keyboardWillShow:")),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: self.view.window)
+        NotificationCenter.default.addObserver(self,
+                                               selector: Selector(("keyboardWillHide:")),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: self.view.window)
+    }
+
+    func removeKeyboardObserver() {
+        NotificationCenter.default.removeObserver(self,
+                                                  name: UIResponder.keyboardWillShowNotification,
+                                                  object: self.view.window)
+        NotificationCenter.default.removeObserver(self,
+                                                  name: UIResponder.keyboardWillHideNotification,
+                                                  object: self.view.window)
+    }
 }
 
 // MARK: - UIViewControllerDelegate
